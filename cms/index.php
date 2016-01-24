f3ae73713b05b7303d94c825eaf554bd691f7aeb
David256 <davidborja456@gmail.com>
2016-01-23 20:55:28 -0500

    cambio en el index y archivo de configuracion agregado

--- cms/index.php
+++ cms/index.php
@@ -12,3 +12,40 @@ if(isset($_SESSION["id"])){
 }
 ?>
 
+<!-- Let's G0! -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Code Reactor</title>
+</head>
+<body>
+	<header>
+		<h1>Code Reactor</h1>
+	</header>
+	<nav>
+		<ul>
+			<li>
+			<a href="index.php">INICIO</a>
+			</li>
+			<?php 
+			if ($root){
+				echo "<li>\r\n";
+				echo '<a href="logout.php" title="Salir" >Salir</a>';
+				echo "</li>\r\n";
+			}else{
+				echo "<li>\r\n";
+				echo '<a href="login.php" title="Entrar" >Entrar</a>';
+				echo "</li>\r\n";
+			}
+			 ?>
+		</ul>
+	</nav>
+	<section>
+		<article></article>
+	</section>
+	<footer>
+		<p>El material publicado en este sitio pertenece a CodeReactor, si es de uso libre se avisará, de lo contrario todos los derechos están reservados</p>
+	</footer>
+</body>
+</html>
---
+++ cms/mundiales.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+* Definimos todas ls variables usadas en toda la web
+*
+* En este archivo están todas las variables, como titulo de la página,
+* texto dle pie de página, mensaje a ciertos errores, etc.
+*
+*/
+
+// este es el titulo del sitio web y el que saldrá en la cabecera de la página
+$Title = "Code Reactor ☢";
+
+// este es el mensaje que estará dentro de la etiqueta <meta> y saldrá en el resultado de busqueda de Google, Bing, etc.
+$Description = "Sitio web Code Reactor, ven y visitanos";
+
+// Este es el mensaje de error que saldrá cuando se intenta buscar un post que no existe
+
+$NotFound = "El tal sitio que busca, no existe :P";
+
+
